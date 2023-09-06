47395338e19663e87991c529f1aa4d77748d44ed
cthed2 <cristhian.ipanaque@gmail.com>
2023-09-05 19:14:40 -0500

    Actualice para hacer un post

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hola Mundo desde Flask!"
+    return "API para recibir datos desde una página web!"
+
+@app.route('/data', methods=['POST'])
+def receive_data():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+
+    with open('data.txt', 'a') as f:
+        f.write(f"Name: {name}, Email: {email}, Message: {message}\n")
+    
+    return jsonify({"message": "Datos almacenados correctamente!"}), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
